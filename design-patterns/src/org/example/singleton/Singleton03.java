b1da8d898f4361e3457acf010d652a7566eea7be
peiyiqiang <pyq722@163.com>
2020-09-10 17:01:16 +0800

    add

--- design-patterns/src/org/example/singleton/Singleton03.java
+++ design-patterns/src/org/example/singleton/Singleton03.java
@@ -6,20 +6,21 @@ package org.example.singleton;
  * @author pYq
  */
 public class Singleton03 {
-	
+
+	//volatile 解决指令重排造成的问题
 	private static volatile Singleton03 instance;
 	
 	private Singleton03() {	
 	}
 	
 	public static Singleton03 getInstance() {
+		//DCL double checked locking 解决线程不安全的问题
 		if(instance == null) {
 			synchronized (Singleton03.class) {
 				if(instance == null)
-					instance = new  Singleton03();
+					instance = new Singleton03();
 			}
 		}
 		return instance;
 	}
-
 }
